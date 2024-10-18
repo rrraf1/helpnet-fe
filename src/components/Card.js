1627e184a01bad4782ccfb825e386bc2fadf9556
rrraf1 <rafirizqullahramadhan@gmail.com>
2024-10-18 11:17:47 +0700

    feat: draggable card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,22 @@
 import { Typography } from "@mui/material";
 import { Share } from "@mui/icons-material";
+import { useDrag } from "../tools/UseDrag";
+import { useRef, useEffect } from "react";
 
 export default function Card({ text, author, user }) {
+  const draggableRef = useRef(null);
+
+  const { position, handleMouseDown, setInitialPosition } = useDrag({
+    ref: draggableRef,
+  });
+
+  useEffect(() => {
+    if (draggableRef.current) {
+      const rect = draggableRef.current.getBoundingClientRect();
+      setInitialPosition(rect.left, rect.top);
+    }
+  }, [setInitialPosition]);
+
   const shareImage = () => {
     if (user == null) {
       alert("You must be logged in to share this post");
@@ -11,7 +26,15 @@ export default function Card({ text, author, user }) {
   };
 
   return (
-    <div className="card-container">
+    <div
+      className="card-container"
+      ref={draggableRef}
+      style={{
+        transform: `translate(${position.x}px, ${position.y}px)`,
+        cursor: "move",
+      }}
+      onMouseDown={handleMouseDown}
+    >
       <div className="content-wrapper">
         <Typography variant="h2" className="card__text">
           {text}
